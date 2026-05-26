aae72f5144d9037dc404c2841f7b4cb5719b3342
MangousteEnFeu <161948048+MangousteEnFeu@users.noreply.github.com>
2026-05-26 15:16:05 +0200

    Refactoring: remplacement de Map<String, Object> par ParentDroitAllocationRequest

--- src/main/java/ch/hearc/cafheg/domain/allocations/ParentDroitAllocationRequest.java
+++ src/main/java/ch/hearc/cafheg/domain/allocations/ParentDroitAllocationRequest.java
@@ -11,68 +11,104 @@ public class ParentDroitAllocationRequest {
     private boolean parentsEnsemble;
     private BigDecimal parent1Salaire;
     private BigDecimal parent2Salaire;
+    private boolean parent1AutoriteParentale;
+    private boolean parent2AutoriteParentale;
+    private boolean parent1Independant;
+    private boolean parent2Independant;
 
     public String getEnfantResidence() {
         return enfantResidence;
     }
 
-    public void setEnfantResidence(String v) {
-        this.enfantResidence = v;
+    public void setEnfantResidence(String enfantResidence) {
+        this.enfantResidence = enfantResidence;
     }
 
     public boolean isParent1ActiviteLucrative() {
         return parent1ActiviteLucrative;
     }
 
-    public void setParent1ActiviteLucrative(boolean v) {
-        this.parent1ActiviteLucrative = v;
+    public void setParent1ActiviteLucrative(boolean parent1ActiviteLucrative) {
+        this.parent1ActiviteLucrative = parent1ActiviteLucrative;
     }
 
     public String getParent1Residence() {
         return parent1Residence;
     }
 
-    public void setParent1Residence(String v) {
-        this.parent1Residence = v;
+    public void setParent1Residence(String parent1Residence) {
+        this.parent1Residence = parent1Residence;
     }
 
     public boolean isParent2ActiviteLucrative() {
         return parent2ActiviteLucrative;
     }
 
-    public void setParent2ActiviteLucrative(boolean v) {
-        this.parent2ActiviteLucrative = v;
+    public void setParent2ActiviteLucrative(boolean parent2ActiviteLucrative) {
+        this.parent2ActiviteLucrative = parent2ActiviteLucrative;
     }
 
     public String getParent2Residence() {
         return parent2Residence;
     }
 
-    public void setParent2Residence(String v) {
-        this.parent2Residence = v;
+    public void setParent2Residence(String parent2Residence) {
+        this.parent2Residence = parent2Residence;
     }
 
     public boolean isParentsEnsemble() {
         return parentsEnsemble;
     }
 
-    public void setParentsEnsemble(boolean v) {
-        this.parentsEnsemble = v;
+    public void setParentsEnsemble(boolean parentsEnsemble) {
+        this.parentsEnsemble = parentsEnsemble;
     }
 
     public BigDecimal getParent1Salaire() {
         return parent1Salaire;
     }
 
-    public void setParent1Salaire(BigDecimal v) {
-        this.parent1Salaire = v;
+    public void setParent1Salaire(BigDecimal parent1Salaire) {
+        this.parent1Salaire = parent1Salaire;
     }
 
     public BigDecimal getParent2Salaire() {
         return parent2Salaire;
     }
 
-    public void setParent2Salaire(BigDecimal v) {
-        this.parent2Salaire = v;
+    public void setParent2Salaire(BigDecimal parent2Salaire) {
+        this.parent2Salaire = parent2Salaire;
+    }
+
+    public boolean isParent1AutoriteParentale() {
+        return parent1AutoriteParentale;
+    }
+
+    public void setParent1AutoriteParentale(boolean parent1AutoriteParentale) {
+        this.parent1AutoriteParentale = parent1AutoriteParentale;
+    }
+
+    public boolean isParent2AutoriteParentale() {
+        return parent2AutoriteParentale;
+    }
+
+    public void setParent2AutoriteParentale(boolean parent2AutoriteParentale) {
+        this.parent2AutoriteParentale = parent2AutoriteParentale;
+    }
+
+    public boolean isParent1Independant() {
+        return parent1Independant;
+    }
+
+    public void setParent1Independant(boolean parent1Independant) {
+        this.parent1Independant = parent1Independant;
+    }
+
+    public boolean isParent2Independant() {
+        return parent2Independant;
+    }
+
+    public void setParent2Independant(boolean parent2Independant) {
+        this.parent2Independant = parent2Independant;
     }
 }
